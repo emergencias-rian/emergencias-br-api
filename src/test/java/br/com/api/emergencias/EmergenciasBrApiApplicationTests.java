52de93e548f946b9ef9f5253cd934cc2f456096f
Felipe Alves <felipe.barbosa@ext.valid.com>
2025-05-21 21:56:01 -0300

    First commit

---
+++ src/test/java/br/com/api/emergencias/EmergenciasBrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.emergencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergenciasBrApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
